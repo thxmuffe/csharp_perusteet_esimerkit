b0834be683e3e96e4fe317c4817b14096491010e
Paavo <paavo.penttila@futunio.fi>
2020-06-08 11:36:39 +0300

    Add project for compiling

---
+++ Various/VariableScope.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class VariableScope
+{
+	public int Example1()
+	{
+		var x = 1;
+
+		if (x == 1)
+		{
+			var y = 0;
+		}
+
+		return x + y;
+	}
+
+	public int Example2()
+	{
+		var x = 3;
+		return x;
+	}
+}
