72603916c398b328828bc5dacebce868b354314d
Paavo <paavo.penttila@futunio.fi>
2020-06-08 16:48:25 +0300

    Instance and class method example

---
+++ Various/InstanceMethodsExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class InstanceMethodExample
+{
+	// absence of static-keyword makes this an instance method (default)
+	public void CallInstance()
+	{
+		Console.WriteLine("Instance!");
+	}
+
+	// static keyword makes this a class-method
+	public static void CallClass()
+	{
+		Console.WriteLine("Classy!");
+	}
+}
